701e0a9c44929b44a1fe74d3c1007f98fb9f362b
Vinayak Kalaskar <vinayak1592@gmail.com>
2017-06-02 16:57:40 -0700

    Committing code + doc

---
+++ SortUniqueWords.java
@@ -0,0 +1,107 @@
+package com.example;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+
+public class SortUniqueWords {
+
+
+    public static void main(String[] args) {
+
+        String input = "This is a test example.... A short example of a valid input.";
+
+        try {
+            for (Map.Entry<String, Integer> entry : new SortUniqueWords().printOccurencesOfWordsInString(input)) {
+                System.out.println(entry.getKey() + " " + entry.getValue());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    /**
+     * Takes path of the file as input, reads all lines from the file and prints unique words along with the number of occurrences
+     *
+     * @param path A valid path to the file
+     * @throws IOException
+     */
+    public void printOccurencesOfWordsInFile(String path) throws IOException {
+
+        Map<String, Integer> occurrences = new HashMap<>();
+
+        //Put unique words in a map maintaining the count of occurrences
+        for (String line : Files.readAllLines(Paths.get(path))) {
+            for (String s : line.toLowerCase().replaceAll("[^a-zA-Z ]+", "").split(" ")) {
+                if (occurrences.containsKey(s)) {
+                    occurrences.put(s, occurrences.get(s) + 1);
+                } else {
+                    occurrences.put(s, 1);
+                }
+            }
+        }
+
+        //Put all the entries in a list to sort
+        List<Map.Entry<String, Integer>> sortedOccurrences = new LinkedList<>(occurrences.entrySet());
+
+        //Custom comparator to sort the list first by number of occurrences and then by value.
+        Collections.sort(sortedOccurrences, new Comparator<Map.Entry<String, Integer>>() {
+
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                if (o1.getValue().compareTo(o2.getValue()) == 0) {
+                    return (o1.getKey().compareTo(o2.getKey()));
+                } else {
+                    return (o2.getValue().compareTo(o1.getValue()));
+                }
+            }
+        });
+
+        for (Map.Entry<String, Integer> entry : sortedOccurrences) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+
+    }
+
+
+    /**
+     * Function to test the code directly from string input (used by Junit test)
+     *
+     * @param input A string input to extract unique words from.
+     * @return
+     */
+    public List<Map.Entry<String, Integer>> printOccurencesOfWordsInString(String input) {
+
+        Map<String, Integer> countMap = new HashMap<String, Integer>();
+
+
+        for (String s : input.toLowerCase().replaceAll("[^a-zA-Z ]+", "").split(" ")) {
+            if (countMap.containsKey(s)) {
+                countMap.put(s, countMap.get(s) + 1);
+            } else {
+                countMap.put(s, 1);
+            }
+        }
+
+        List<Map.Entry<String, Integer>> lst = new LinkedList<>(countMap.entrySet());
+
+        Collections.sort(lst, new Comparator<Map.Entry<String, Integer>>() {
+
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                if (o1.getValue().compareTo(o2.getValue()) == 0) {
+                    return (o1.getKey().compareTo(o2.getKey()));
+                } else {
+                    return (o2.getValue().compareTo(o1.getValue()));
+                }
+            }
+        });
+
+        return lst;
+
+    }
+}
---
+++ SortUniqueWordsTest.java
@@ -0,0 +1,28 @@
+import com.company.SortUniqueWords;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class SortUniqueWordsTest {
+    @Test
+    public void countOccurenceUnitTest() {
+        String input = "This is a test example.... A short example of a valid input.";
+
+        List<Map.Entry<String, Integer>> sortedList = new SortUniqueWords().printOccurencesOfWordsInString(input);
+
+        assertEquals(sortedList.get(0).getKey(), "a");
+        assertEquals(sortedList.get(1).getKey(), "example");
+        assertEquals(sortedList.get(2).getKey(), "input");
+        assertEquals(sortedList.get(3).getKey(), "is");
+        assertEquals(sortedList.get(4).getKey(), "of");
+        assertEquals(sortedList.get(5).getKey(), "short");
+        assertEquals(sortedList.get(6).getKey(), "test");
+        assertEquals(sortedList.get(7).getKey(), "this");
+        assertEquals(sortedList.get(8).getKey(), "valid");
+
+
+    }
+}
